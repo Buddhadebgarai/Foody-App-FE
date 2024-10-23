10f3015aa34b599d8ec2d980bdf43929c79afe8d
Buddhadeb Garai <buddhadebgarai2016@gmail.com>
2024-10-24 00:31:09 +0530

    Tested Restaurant Listing MS

--- src/app/restaurant-listing/componets/restaurant-listing.component.ts
+++ src/app/restaurant-listing/componets/restaurant-listing.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RestaurantService } from '../service/restaurant.service';
+import { restaurant } from 'src/app/shared/model/restaurant';
 
 @Component({
   selector: 'app-restaurant-listing',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./restaurant-listing.component.css']
 })
 export class RestaurantListingComponent {
+
+  public restaurantList: restaurant[];
+  ngOnInit()
+  {
+    this.getRestaurantList();
+  }
+  getRestaurantList() {
+    this.restaurentService.getAllRestaurants().subscribe((data:any)=>{
+      this.restaurantList=data;
+    })
+  }
+
+  constructor(private router:Router, private restaurentService:RestaurantService)
+  {
+
+  }
+  getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
+  getRandomImage(): string {
+    const imageCount = 8; // Adjust this number based on the number of images in your asset folder
+    const randomIndex = this.getRandomNumber(1, imageCount);
+    return `${randomIndex}.jpg`; // Replace with your image filename pattern
+  }
+
+  onButtonClick(id: any) {
+    this.router.navigate(['/food-catalogue', id]);
+  }
+
+
 
 }
